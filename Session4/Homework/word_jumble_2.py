d4176e2fe0cf260db633797c6998ef5f76497b2b
leducanh300795 <leducanh300795@gmail.com>
2018-04-19 23:59:38 +0700

    intial commit

--- Session4/Homework/word_jumble_2.py
+++ Session4/Homework/word_jumble_2.py
@@ -1,17 +1,20 @@
 from random import choice
-a = "champion"
-b = list(a)
-for i in range(len(a)):
-    c = choice(b)
-    print(c, end = " ")
-    b.remove(c)
-print()
+x = ["champion","meticulous","hexagon"]
+for _ in range(len(x)):
+    a = choice(x)
+    b = list(a)
+    x.remove(a)
+    for i in range(len(a)):
+        c = choice(b)
+        print(c, end = " ")
+        b.remove(c)
+    print()
 
-run = True
-while run:
-    e = input("Your Answer: ")
-    if e == a:
-        print("Hura")
-        run = False
-    else:
-        print("Again :(!")
+    run = True
+    while run:
+        e = input("Your Answer: ")
+        if e == a:
+            print("Hura")
+            run = False
+        else:
+            print("Again :(!")
